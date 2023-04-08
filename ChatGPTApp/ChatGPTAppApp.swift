145b3b80541be5eaf111207d400e49a522372796
veerkalaria <veerkalaria27@gmail.com>
2023-04-08 09:31:48 +0530

    Initial Commit

---
+++ ChatGPTApp/ChatGPTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAppApp.swift
+//  ChatGPTApp
+//
+//  Created by veer kalaria on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
